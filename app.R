e7c717042a6ae1022b31c540ae5330b508c5c213
tslarue <33507294+tslarue@users.noreply.github.com>
2022-06-26 18:13:26 -0700

    edit all replicate view

--- app.R
+++ app.R
@@ -69,48 +69,104 @@ server <- function(input, output) {
   output$text1 <- renderText({input$access1})
   output$access1 <- renderPlot({
     
-    plotall <- cleaned_root %>% 
+    plot6 <- cleaned_root %>% 
       filter(accession == input$access1,
-             experiment %in% c("HL06", "HL11", "HL15")) %>% 
+             experiment %in% c("HL06")) %>% 
       ggplot(aes(start_x,start_y)) + 
       geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
       xlim(0,15) + 
       scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
       coord_fixed(ratio = 1) + 
-      ggtitle("All replicates") +
+      ggtitle("All HL06") +
       xlab("Width (cm)") + 
       ylab("Depth (cm)") + 
-      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "bottom")
+      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "none")
+    plot11 <- cleaned_root %>% 
+      filter(accession == input$access1,
+             experiment %in% c("HL11")) %>% 
+      ggplot(aes(start_x,start_y)) + 
+      geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
+      xlim(0,15) + 
+      scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
+      coord_fixed(ratio = 1) + 
+      ggtitle("All HL11") +
+      xlab("Width (cm)") + 
+      ylab("Depth (cm)") + 
+      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "none")
+    plot15_stock <- cleaned_root %>% 
+      filter(accession == input$access1,
+             experiment %in% c("HL15")) %>% 
+      ggplot(aes(start_x,start_y)) + 
+      geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
+      xlim(0,15) + 
+      scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
+      coord_fixed(ratio = 1) + 
+      ggtitle("All HL15") +
+      xlab("Width (cm)") + 
+      ylab("Depth (cm)") + 
+      theme_set(theme_cowplot()) + theme_tsl()
+    plot15 <- plot15_stock + theme(legend.position = "none")
+    legend <- get_legend(plot15_stock + theme(legend.position = "bottom"))
+    plots <- plot_grid(plot6, plot11, plot15, ncol = 3)
+    plotall <- plot_grid(plots, legend, nrow = 2, rel_heights = c(1, 0.2))
     
     plot1 <- shinyplot(cleaned_root, input$access1, "HL06")
     plot2 <- shinyplot(cleaned_root, input$access1, "HL11")
     plot3 <- shinyplot(cleaned_root, input$access1, "HL15")
     
     plotrep <- plot_grid(plot1, plot2, plot3, nrow = 3)
-    plot_grid(plotall, plotrep, nrow =1, rel_widths = c(1, 3))
+    plot_grid(plotall, plotrep, nrow =1, rel_widths = c(2, 3))
   })
   output$text2 <- renderText({input$access2})
   output$access2 <- renderPlot({
     
-    plotall <- cleaned_root %>% 
+    plot6 <- cleaned_root %>% 
       filter(accession == input$access2,
-             experiment %in% c("HL06", "HL11", "HL15")) %>% 
+             experiment %in% c("HL06")) %>% 
       ggplot(aes(start_x,start_y)) + 
       geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
       xlim(0,15) + 
       scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
       coord_fixed(ratio = 1) + 
-      ggtitle("All replicates") +
+      ggtitle("All HL06") +
       xlab("Width (cm)") + 
       ylab("Depth (cm)") + 
-      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "bottom")
+      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "none")
+    plot11 <- cleaned_root %>% 
+      filter(accession == input$access2,
+             experiment %in% c("HL11")) %>% 
+      ggplot(aes(start_x,start_y)) + 
+      geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
+      xlim(0,15) + 
+      scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
+      coord_fixed(ratio = 1) + 
+      ggtitle("All HL11") +
+      xlab("Width (cm)") + 
+      ylab("Depth (cm)") + 
+      theme_set(theme_cowplot()) + theme_tsl() + theme(legend.position = "none")
+    plot15_stock <- cleaned_root %>% 
+      filter(accession == input$access2,
+             experiment %in% c("HL15")) %>% 
+      ggplot(aes(start_x,start_y)) + 
+      geom_segment(aes(xend=end_x,yend=end_y, color=hours),arrow = arrow(length = unit(0.01, "npc")),alpha=1) + 
+      xlim(0,15) + 
+      scale_y_reverse(limits=c(30,0)) + scale_color_gradient(low="darkred", high="gold", limits = c(0, 768)) + 
+      coord_fixed(ratio = 1) + 
+      ggtitle("All HL15") +
+      xlab("Width (cm)") + 
+      ylab("Depth (cm)") + 
+      theme_set(theme_cowplot()) + theme_tsl()
+    plot15 <- plot15_stock + theme(legend.position = "none")
+    legend <- get_legend(plot15_stock + theme(legend.position = "bottom"))
+    plots <- plot_grid(plot6, plot11, plot15, ncol = 3)
+    plotall <- plot_grid(plots, legend, nrow = 2, rel_heights = c(1, 0.2))
     
     plot1 <- shinyplot(cleaned_root, input$access2, "HL06")
     plot2 <- shinyplot(cleaned_root, input$access2, "HL11")
     plot3 <- shinyplot(cleaned_root, input$access2, "HL15")
     
     plotrep <- plot_grid(plot1, plot2, plot3, nrow = 3)
-    plot_grid(plotall, plotrep, nrow =1, rel_widths = c(1, 3))
+    plot_grid(plotall, plotrep, nrow =1, rel_widths = c(2, 3))
   })
 }
 
